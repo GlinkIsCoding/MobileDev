26f7f0493a8b1b64f4ecea6bfe61abdca2f85636
GlinkIsCoding <carlos.espino.m@gmail.com>
2021-03-11 19:52:29 -0600

    Añadir managers y toggle de visibilidad de imagen

--- example1/app/src/main/java/com/example/ex1/MainActivity.java
+++ example1/app/src/main/java/com/example/ex1/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.ex1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonManager;
+    TextView txtViewManager;
+    ImageView imgViewManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonManager = findViewById(R.id.button);
+        txtViewManager = findViewById(R.id.textView);
+        imgViewManager = findViewById(R.id.imageView);
+
+        buttonManager.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (imgViewManager.getVisibility() == View.VISIBLE){
+                    imgViewManager.setVisibility(View.INVISIBLE);
+                }
+                else {
+                    imgViewManager.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 }
