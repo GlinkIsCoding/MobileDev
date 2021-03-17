88ce5622177eb93ee4a8e163b4ed600c38308b51
GlinkIsCoding <carlos.espino.m@gmail.com>
2021-03-16 20:21:05 -0600

    Añadir notificaciones con Intent a nueva Actividad

--- example3/app/src/main/java/com/example/example3/MainActivity.java
+++ example3/app/src/main/java/com/example/example3/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.example3;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -12,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     Context myContext;
     Toast myToast;
+    Intent myIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +56,20 @@ public class MainActivity extends AppCompatActivity {
                 });
         AlertDialog myDialog = dialogConf.create();
         myDialog.show();
+
+
+        Notification.Builder notifConf = new Notification.Builder(this);
+        notifConf.setContentTitle("This is my notification!");
+        notifConf.setContentText("This is the detail of the notification");
+        notifConf.setSmallIcon(R.mipmap.ic_launcher);
+
+        Intent myIntent = new Intent (this, ChildActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(myContext, 0, myIntent, 0);
+        notifConf.setContentIntent(contentIntent);
+
+        NotificationManager myNotification = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        myNotification.notify(1234, notifConf.build());
+
 
     }
 }
