5be3af46b018f0524387017b99ed25f8bbb19240
GlinkIsCoding <carlos.espino.m@gmail.com>
2021-03-16 20:08:55 -0600

    Añadir Dialog con Botones neutral, positivo y negativo.

--- example3/app/src/main/java/com/example/example3/MainActivity.java
+++ example3/app/src/main/java/com/example/example3/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.example3;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -21,6 +23,34 @@ public class MainActivity extends AppCompatActivity {
         myToast = new Toast(myContext);
 
         myToast.makeText(myContext, "It's a me! A toast!", Toast.LENGTH_LONG).show();
+
+
+        AlertDialog.Builder dialogConf = new AlertDialog.Builder(this);
+        dialogConf.setTitle("This is a neutral dialog");
+        dialogConf.setMessage("This is the Text of the Neutral Dialog");
+        dialogConf.setIcon(R.mipmap.ic_launcher);
+
+        dialogConf.setNeutralButton("Dismiss",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        myToast.makeText(myContext, "You clicked Dismiss", Toast.LENGTH_LONG).show();
+                    }
+                });
+        dialogConf.setPositiveButton("Yes",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        myToast.makeText(myContext, "You clicked Yes", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+        dialogConf.setNegativeButton("No",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        myToast.makeText(myContext, "You clicked No", Toast.LENGTH_LONG).show();
+                    }
+                });
+        AlertDialog myDialog = dialogConf.create();
+        myDialog.show();
 
     }
 }
