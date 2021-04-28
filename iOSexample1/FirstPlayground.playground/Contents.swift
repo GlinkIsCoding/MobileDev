2b23b24180bcc3bfbb3215c1326e484f017dd216
GlinkIsCoding <carlos.espino.m@gmail.com>
2021-04-27 20:30:56 -0500

    First iOS Playground example

---
+++ iOSexample1/FirstPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+// Optionals.
+
+var optional: String = "something"
+
+if(optional != nil){
+    print(optional)
+}
+else{
+    print("Error: Optional is nil")
+}
+
+print(optional)
